1965d935eabea46776b47ab5c828d52da7e783eb
Alex Nguyen <dungtn@gmail.com>
2018-03-30 17:00:18 +0700

    init some pseudo functions

--- vyper-contracts/lazy_loose_money.v.py
+++ vyper-contracts/lazy_loose_money.v.py
@@ -8,6 +8,26 @@ Closed: event({ _burnablePaymentId: indexed(int128) })
 
 # ***** Data *****
 
+commitments: public({
+    title: bytes <= 300,
+    owner: address,
+    guardian: address, # the one who report everyday commitment
+
+    supporters_funds: wei_value[address], # supporters will add funds to encourage commitment owner
+    deposit: wei_value, # commitment owner need to deposit money in
+    amount_burned: wei_value,
+    _balance: wei_value, # = deposit + supporters_funds.sum - amount_burned
+
+    days: int128,
+    started_at: timestamp,
+    daily_reports: {
+        completed: bool, # guardian report if the commitment for today is completed or not
+        complained: bool, # owner can complain about daily_report correctness or not
+    }[int128],
+
+    state: int128,
+}[address])
+
 State: public({
     Opened: int128,
     Committed: int128,
@@ -25,9 +45,32 @@ def __init__():
 
 # ***** Helpers *****
 
+@public
+@constant
+def days_passed(committer: address) -> int128:
+    # block.timestamp is a uint256 value in seconds since the epoch
+    # return floor(decimal(block.timestamp - self.commitments[committer].started_at) / 86400) # 24*60*60 seconds
+    return 0
 
-# ***** Public *****
+@public
+@constant
+def guardian_daily_reward(committer: address) -> wei_value:
+    # self.commitments[committer]._balance /
+    return 0
+
 @public
 @constant
 def version() -> int128:
     return 1
+
+# ***** Public *****
+@public
+@payable
+def create_commitment() -> bool:
+    committer: address = msg.sender
+    if (self.commitments[committer].owner != 0x0000000000000000000000000000000000000000
+            and self.commitments[msg.sender].state != self.State.Closed):
+        return False
+    else:
+        self.commitments[committer].owner = committer
+        return True
