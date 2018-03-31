8ad4af9e353e20bea30a99c0b2c89b1e88850e71
Alex Nguyen <dungtn@gmail.com>
2018-03-31 10:57:40 +0700

    introduce wait_for_transaction() and assert

--- deploy_and_test.py
+++ deploy_and_test.py
@@ -6,29 +6,24 @@ Remember to run `ganache-cli -p 7545`
 '''
 web3 = Web3(HTTPProvider('http://localhost:7545'))
 
+def wait_for_transaction(tx_hash):
+    for i in range(0, 5):
+        try:
+            tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+            return tx_receipt
+        except:
+            print("Reading failure for {} time(s)".format(i + 1))
+            sleep(5+i)
+    raise Exception("Cannot wait for transaction")
+
 def deploy_contract(**config):
     deployer = web3.personal.listAccounts[0]
     contract = web3.eth.contract(abi=config['abi'], bytecode=config['bytecode'])
     tx_hash = contract.constructor().transact(transaction={'from': deployer, 'gas': 910000})
-
-    # Waiting for contract to be deployed
-    i = 0
-    while i < 5:
-        try:
-            # Get tx receipt to get contract address
-            tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-            contract_address = tx_receipt['contractAddress']
-            break  # if success, then exit the loop
-        except:
-            print("Reading failure for {} time(s)".format(i + 1))
-            sleep(5+i)
-            i = i + 1
-            if i >= 5:
-                 raise Exception("Cannot wait for contract to be deployed")
-
+    tx_receipt = wait_for_transaction(tx_hash)
+    contract_address = tx_receipt['contractAddress']
     if contract_address:
         print('Contract deployed at:', contract_address)
-
     contract_instance = web3.eth.contract(abi=config['abi'], address=contract_address)
     return contract_instance
 
@@ -43,10 +38,20 @@ compiled_llm_factory = compiled['<stdin>:LlmFactory']
 compiled_commitment = compiled['<stdin>:Commitment']
 # print(compiled_commitment)
 
-committer = web3.personal.listAccounts[1]
 llm_factory = deploy_contract(abi=compiled_llm_factory['abi'], bytecode=compiled_llm_factory['bin'])
-txn = llm_factory.functions.createCommitment("my commitment for next 30 days", 30).transact({'from': committer, 'value': web3.toWei(50000, 'gwei')})
+
+committer = web3.personal.listAccounts[1]
+title = "my commitment for next 30 days"
+days = 30
+
+tx_hash = llm_factory.functions.createCommitment(title, days).transact({'from': committer, 'value': web3.toWei(50000, 'gwei')})
+tx_receipt = wait_for_transaction(tx_hash)
+
 commitment_address = llm_factory.functions.getLastCommitmentAddress().call()
 print('Get commitment contract at', commitment_address)
+
 commitment_contract = web3.eth.contract(abi = compiled_commitment['abi'], address = commitment_address)
-print(commitment_contract.functions.getInfo().call())
+(_owner, _title, _days) = commitment_contract.functions.getInfo().call()
+assert _owner == committer
+assert _title == title
+assert _days == days
