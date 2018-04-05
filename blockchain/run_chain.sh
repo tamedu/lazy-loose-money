4d00d040dfaf46b50e5d1ae9b059858303801d3d
Alex Nguyen <dungtn@gmail.com>
2018-04-06 06:20:51 +0700

    add ref config

--- blockchain/run_chain.sh
+++ blockchain/run_chain.sh
@@ -1,1 +1,2 @@
+# Ref https://github.com/f-o-a-m/cliquebait/blob/master/run.bash
 geth --rpc --rpcaddr 127.0.0.1 --rpcport 8545 --rpcapi admin,debug,eth,miner,net,personal,shh,txpool,web3,ws --ws --wsaddr 127.0.0.1 --wsport 8546 --wsapi admin,debug,eth,miner,net,personal,shh,txpool,web3,ws --datadir ./chain_data --maxpeers 0 --networkid 1234 --port 30303 --nodiscover --mine --minerthreads 1
